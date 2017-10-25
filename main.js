603a7b577d1aa473b8cb009766d99f6ac50880d7
Abby Starnes <abbymstarnes@gmail.com>
2017-10-25 01:12:45 -0400

    initial commit, html, accessiblity, BEM

---
+++ main.js
@@ -0,0 +1,37 @@
+// set no js as default before document.loaded
+
+let tbody = `<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>
+<tr>
+  <td></td>
+  <td></td>
+  <td></td>
+</tr>`;
+
+
+// toggle aria expanded on nav button
+// var toggle = document.querySelector('#toggle');
+// var menu = document.querySelector('#menu');
+//
+// toggle.addEventListener('click', function(){
+//   if (menu.classList.contains('is-active')) {
+//     this.setAttribute('aria-expanded', 'false');
+//     menu.classList.remove('is-active');
+//   } else {
+//     menu.classList.add('is-active');
+//     this.setAttribute('aria-expanded', 'true');
+//   }
+// });
