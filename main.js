793c16dfd0c7f35005c851d3ebb0cfefd1d8ac03
Abby Starnes <abbymstarnes@gmail.com>
2017-10-25 21:56:04 -0400

    js functionality mobile nav, login dialog

--- main.js
+++ main.js
@@ -2,11 +2,52 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
-// hide hidden content if js loads
-let activeElems = document.querySelectorAll('.is-active');
+// hide hidden content if js loads, show nav btn
+const activeElems = document.querySelectorAll('.is-active');
 for (let i = 0; i < activeElems.length; i++){
   activeElems[i].classList.remove('is-active');
 }
+const menuBtn = document.getElementById('menu-btn');
+menuBtn.classList.remove('is-hidden');
+
+// toggle navigation, focus on first nav elem or h1/logo for accessibility
+menuBtn.addEventListener('click', ()=>{
+  const mainNav = document.getElementById('c-nav-links');
+  const logo = document.getElementById('logo');
+  mainNav.classList.toggle('is-active');
+  if (mainNav.classList.contains('is-active')){
+    mainNav.firstElementChild.focus();
+  } else {
+    logo.focus();
+  }
+});
+
+// show/close login dialog
+const loginBtn = document.getElementById('btn-login');
+const dialog = document.getElementById('dialog');
+const overlay = document.getElementById('overlay');
+
+loginBtn.addEventListener('click', ()=>{
+  overlay.classList.toggle('is-active');
+  dialog.classList.toggle('is-active');
+});
+
+// close dialog w/overlay
+overlay.addEventListener('click', () => {
+  event.preventDefault();
+  dialog.classList.toggle('is-active');
+  overlay.classList.toggle('is-active');
+});
+
+// close dialog w/x button
+const closeBtn = document.getElementById('btn-close-dialog');
+closeBtn.addEventListener('click', () => {
+  event.preventDefault();
+  dialog.classList.toggle('is-active');
+  overlay.classList.toggle('is-active');
+});
+
+
 
 // fill table with json company data
 const tbody = document.getElementById('js-tbody');
@@ -25,27 +66,6 @@ for (let i = 0; i < data.length; i++){
     tbody.append(trow);
   }
 }
-
-// let tbody = `<tr>
-//   <td></td>
-//   <td></td>
-//   <td></td>
-// </tr>
-// <tr>
-//   <td></td>
-//   <td></td>
-//   <td></td>
-// </tr>
-// <tr>
-//   <td></td>
-//   <td></td>
-//   <td></td>
-// </tr>
-// <tr>
-//   <td></td>
-//   <td></td>
-//   <td></td>
-// </tr>`;
 
 
 // toggle aria expanded on nav button
