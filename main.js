dad9bc0183e3b2fb2cf958cf6e7a678fc4813e97
Abby Starnes <abbymstarnes@gmail.com>
2017-10-25 20:40:35 -0400

    mobile styling

--- main.js
+++ main.js
@@ -2,33 +2,50 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
 
+// hide hidden content if js loads
 let activeElems = document.querySelectorAll('.is-active');
-console.log(activeElems);
-
 for (let i = 0; i < activeElems.length; i++){
   activeElems[i].classList.remove('is-active');
 }
 
-let tbody = `<tr>
-  <td></td>
-  <td></td>
-  <td></td>
-</tr>
-<tr>
-  <td></td>
-  <td></td>
-  <td></td>
-</tr>
-<tr>
-  <td></td>
-  <td></td>
-  <td></td>
-</tr>
-<tr>
-  <td></td>
-  <td></td>
-  <td></td>
-</tr>`;
+// fill table with json company data
+const tbody = document.getElementById('js-tbody');
+for (let i = 0; i < data.length; i++){
+  let trow = document.createElement('tr');
+  Object.values(data[i]).forEach((value) => {
+    let cell = document.createElement('td');
+    cell.innerHTML = value;
+    trow.append(cell);
+  });
+  trow.children[1].innerHTML = trow.children[1].innerHTML  + ' %';
+  trow.children[2].innerHTML = '$' + parseInt(trow.children[2].innerHTML).toFixed(2);
+  if (trow.firstElementChild.innerHTML === 'URBank'){
+    tbody.prepend(trow);
+  } else {
+    tbody.append(trow);
+  }
+}
+
+// let tbody = `<tr>
+//   <td></td>
+//   <td></td>
+//   <td></td>
+// </tr>
+// <tr>
+//   <td></td>
+//   <td></td>
+//   <td></td>
+// </tr>
+// <tr>
+//   <td></td>
+//   <td></td>
+//   <td></td>
+// </tr>
+// <tr>
+//   <td></td>
+//   <td></td>
+//   <td></td>
+// </tr>`;
 
 
 // toggle aria expanded on nav button
